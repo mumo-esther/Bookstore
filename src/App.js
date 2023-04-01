d394bee94bda85eeb6897658d21e0b955840c650
mumo-esther <mumoesther2021@gmail.com>
2023-04-01 12:57:43 +0300

    Initialized bookstore CMS and added navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Nav from './components/Nav';
+import Books from './components/Books';
+import './index.css';
+import Categories from './components/Categories';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="books-container">
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/Categories" element={<Categories />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
