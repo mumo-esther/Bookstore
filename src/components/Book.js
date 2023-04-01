057d2eb91966760b9c36772a79481b660aaa4097
mumo-esther <mumoesther2021@gmail.com>
2023-04-01 13:27:13 +0300

    Added bookdata array in books.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ title, author }) => (
+const Book = ({ books }) => (
   <>
-    <h3>{title}</h3>
-    <h4>{author}</h4>
+    {books.map((book) => (
+      <div key={book.id}>
+        <h3>{book.title}</h3>
+        <h4>{book.author}</h4>
+      </div>
+    ))}
   </>
 );
 
 Book.propTypes = {
-  title: PropTypes.string,
-  author: PropTypes.string,
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+    }),
+  ),
 };
 
 Book.defaultProps = {
-  title: '',
-  author: '',
+  books: [],
 };
-export default Book;
+
+const booksData = [
+  { id: 1, title: 'Book title 1', author: 'Book author 1' },
+  { id: 2, title: 'Book title 2', author: 'Book author 2' },
+  { id: 3, title: 'Book title 3', author: 'Book author 3' },
+];
+
+const Books = () => (
+  <div>
+    <Book books={booksData} />
+  </div>
+);
+
+export default Books;
