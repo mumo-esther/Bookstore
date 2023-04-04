a6dd05d729abf3e4c1657ad5909d7c2286c2f1c3
mumo-esther <mumoesther2021@gmail.com>
2023-04-04 22:29:41 +0300

    Added redux in react components

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,22 +1,52 @@
-import { createSlice } from '@reduxjs/toolkit';
-
-const initialState = [];
-
-export const booksSlice = createSlice({
-  name: 'books',
-  initialState,
-  reducers: {
-    addBook: (state, action) => {
-      const { id, title, author } = action.payload;
-      state.push({ id, title, author });
-    },
-    removeBook: (state, action) => {
-      const bookIndex = state.findIndex((book) => book.id === action.payload);
-      state.splice(bookIndex, 1);
-    },
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const books = [
+  {
+    id: 1,
+    author: 'Haruki Murakami',
+    title: 'Kafka on the Shore',
   },
+  {
+    id: 2,
+    author: 'Gabriel Garcia Marquez',
+    title: 'One Hundred Years of Solitude',
+  },
+  {
+    id: 3,
+    author: 'Toni Morrison',
+    title: 'Beloved',
+  },
+  {
+    id: 4,
+    author: 'Octavia Butler',
+    title: 'Kindred',
+  },
+];
+export const addAction = (NewBook) => ({
+  type: ADD_BOOK,
+  payload: NewBook,
+});
+export const removeAction = (id) => ({
+  type: REMOVE_BOOK,
+  id,
 });
 
-export const { addBook, removeBook } = booksSlice.actions;
+const BooksReducer = (state = books, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [
+        ...state,
+        {
+          id: Date.now(),
+          title: action.payload.title,
+          author: action.payload.author,
+        },
+      ];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.id);
+    default:
+      return state;
+  }
+};
 
-export default booksSlice.reducer;
+export default BooksReducer;
